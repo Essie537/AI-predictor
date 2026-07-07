7268052606c5310667d93e6c50fafd1e856bb8b9
Esther Nthenya <esthernthenya537h@gmail.com>
2026-07-07 19:47:43 +0300

    Update website

--- flask_app/app.py
+++ flask_app/app.py
@@ -70,16 +70,6 @@ def predict():
     )
 
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/contact")
-def contact():
-    return render_template("contact.html")
-
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
